52830255faf5abae7eb572efb40d8cabdce38b4b
jeonmmingu <jeonalsrn@gmail.com>
2025-03-09 16:27:17 +0900

    feat: Firebase 초기 설정 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:eng_story/app.dart';
+import 'package:eng_story/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
