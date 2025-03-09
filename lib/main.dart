918d3d507196d548b23a41de65570beab7238a88
jeonmmingu <jeonalsrn@gmail.com>
2025-03-09 17:27:49 +0900

    feat: firebase_services.dart - cloud firestore CRUD 함수 작성
    JsonSerializable abstract class를 정의해서 데이터를 불러오거나 읽을 때 implement 해서 사용

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:eng_story/app.dart';
 import 'package:eng_story/services/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/web.dart';
 
+Logger logger = Logger();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
