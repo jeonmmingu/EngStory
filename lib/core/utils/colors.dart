4eec368d129d017c6a26d8174d76357d7506e93b
jeonmmingu <jeonalsrn@gmail.com>
2025-03-12 19:52:29 +0900

    feat: fonts, images, animations, colors 기본 구성 파일 및 초기 설정 완료

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFFEAF4FC);
+  static const text_1 = Color(0xFF1B1E29);
+  static const text_2 = Color(0xFF4A5568);
+  static const button = Color(0xFF3A7CA5);
+}
---
+++ lib/core/utils/fonts.dart
@@ -0,0 +1,91 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle SejongGeulggot_40_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 40,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_38_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 38,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_36_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 36,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_34_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 34,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_32_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 32,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_30_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_28_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 28,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_26_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 26,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_24_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_22_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 22,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_20_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_18_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_16_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_14_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_12_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_10_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 10,
+    fontWeight: FontWeight.normal,
+  );
+  static const TextStyle SejongGeulggot_8_regular = TextStyle(
+    fontFamily: "SejongGeulggot",
+    fontSize: 8,
+    fontWeight: FontWeight.normal,
+  );
+}
