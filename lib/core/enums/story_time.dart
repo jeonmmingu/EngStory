e3a5bac3637a09688acb9640de430d00503b3b6d
jeonmmingu <jeonalsrn@gmail.com>
2025-03-13 09:54:59 +0900

    feat: home view 기본 구조 구현 완료
    이야기 랜덤 선택 구조 보완 필요, script 페이지 구현 필요

---
+++ lib/core/enums/story_time.dart
@@ -0,0 +1,15 @@
+enum StoryTime {
+  short, // 1~5 min
+  medium, // 5~10 min
+}
+
+extension StoryTimeExtension on StoryTime {
+  String get displayText {
+    switch (this) {
+      case StoryTime.short:
+        return "1-5 분";
+      case StoryTime.medium:
+        return "5-10 분";
+    }
+  }
+}
--- lib/view_models/home_view_model.dart
+++ lib/view_models/home_view_model.dart
@@ -1,3 +1,21 @@
+import 'package:eng_story/core/enums/story_time.dart';
+import 'package:eng_story/models/story.dart';
 import 'package:flutter/material.dart';
 
-class HomeViewModel with ChangeNotifier {}
+class HomeViewModel with ChangeNotifier {
+  StoryTime _storyTime = StoryTime.short;
+  StoryTime get storyTime => _storyTime;
+
+  Story? _selectedStory;
+  Story? get selectedStory => _selectedStory;
+
+  void setStoryTime(StoryTime storyTime) {
+    _storyTime = storyTime;
+    notifyListeners();
+  }
+
+  void setSelectedStory(Story story) {
+    _selectedStory = story;
+    notifyListeners();
+  }
+}
