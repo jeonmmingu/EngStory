c7fa8b4e4a8fcd1de38cb7322d65e22b4853f9f8
jeonmmingu <jeonalsrn@gmail.com>
2025-03-18 14:30:08 +0900

    feat: 읽고 있던 이야기의 index를 local DB에 캐싱 및 StoryView에서 마지막 index 추적 기능 구현
    스토리를 읽을 때 현재 index를 local DB에 저장하도록 설정하고, StoryView에서 마지막으로 본 index를 기반으로 초기화하도록 구현.

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,11 +1,4 @@
-import 'dart:math';
-
 import 'package:eng_story/core/enums/story_time.dart';
-import 'package:eng_story/core/tests/manage_story.dart';
-import 'package:eng_story/core/tests/story/%EB%A1%9C%EC%A0%9C%ED%83%80%20%EC%8A%A4%ED%86%A4%EC%9D%B4%20%ED%95%B4%EB%8F%85%EB%90%9C%20%EA%B3%BC%EC%A0%95.dart';
-import 'package:eng_story/core/tests/story/%EC%84%B8%EA%B3%84%20%EC%B5%9C%EC%B4%88%EC%9D%98%20%EC%8B%A0%EB%AC%B8%EC%9D%80%20%EC%96%B4%EB%96%A4%20%EB%AA%A8%EC%8A%B5%EC%9D%B4%EC%97%88%EC%9D%84%EA%B9%8C.dart';
-import 'package:eng_story/core/tests/story/%EC%9D%B4%EC%8A%A4%ED%84%B0%EC%84%AC%20%EB%AA%A8%EC%95%84%EC%9D%B4%20%EC%84%9D%EC%83%81%EC%9D%98%20%EC%88%A8%EA%B2%A8%EC%A7%84%20%EB%B9%84%EB%B0%80.dart';
-import 'package:eng_story/core/tests/story/%EC%A4%91%EC%84%B8%20%EC%9C%A0%EB%9F%BD%EC%97%90%EC%84%9C%20%EB%A7%88%EB%85%80%20%EC%82%AC%EB%83%A5%EC%9D%B4%20%EB%B2%8C%EC%96%B4%EC%A7%84%20%EC%9D%B4%EC%9C%A0.dart';
 import 'package:eng_story/core/utils/animations.dart';
 import 'package:eng_story/core/utils/colors.dart';
 import 'package:eng_story/core/utils/fonts.dart';
@@ -173,6 +166,9 @@ class HomeView extends StatelessWidget {
               final getScripts = await context
                   .read<StoryViewModel>()
                   .getScripts(homeViewModel.selectedStory!.id);
+              context
+                  .read<StoryViewModel>()
+                  .init(homeViewModel.selectedStory!.lastReadScriptIndex);
               if (getScripts) {
                 context.goNamed("storyView");
               }
@@ -265,6 +261,8 @@ class HomeView extends StatelessWidget {
                 onTap: () async {
                   HapticFeedback.heavyImpact();
                   final story = homeViewModel.getRandomStoryByReadTime();
+                  debugPrint(
+                      "Selected Story: ${story.title} / ${story.id} / ${story.readTime} / ${story.category} / ${story.lastReadScriptIndex}");
                   homeViewModel.setSelectedStory(story);
                 },
                 child: Container(
