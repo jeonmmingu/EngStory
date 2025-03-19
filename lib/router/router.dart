b14118c50f5400e425ff2b9b61ca376d8e35df28
jeonmmingu <jeonalsrn@gmail.com>
2025-03-19 18:33:15 +0900

    feat: DeviceInfoManager 생성 및 Router AdminView 리디렉트 기능 추가
    DeviceInfoManager 클래스를 생성하고, AdminView를 간단하게 추가 (기능 구현 없음).

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,8 +1,15 @@
+import 'package:eng_story/services/local/device_info_manager.dart';
+import 'package:eng_story/views/admin_view.dart';
 import 'package:eng_story/views/home_view.dart';
 import 'package:eng_story/views/story_view.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
+  initialLocation: '/', // 기본 경로 (redirect에서 변경됨)
+  redirect: (context, state) {
+    final bool isAdmin = DeviceInfoManager().isAdmin();
+    return isAdmin ? '/adminView' : '/';
+  },
   routes: [
     GoRoute(
       path: '/',
@@ -10,11 +17,16 @@ final GoRouter router = GoRouter(
       builder: (context, state) => const HomeView(),
       routes: [
         GoRoute(
-          path: '/storyView',
+          path: 'storyView', // '/' 제거 (중첩 라우트에서는 상대 경로)
           name: 'storyView',
           builder: (context, state) => const StoryView(),
-        )
+        ),
       ],
+    ),
+    GoRoute(
+      path: '/adminView',
+      name: 'adminView',
+      builder: (context, state) => const AdminView(),
     ),
   ],
 );
